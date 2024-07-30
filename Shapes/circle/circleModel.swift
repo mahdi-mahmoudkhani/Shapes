1928553a340cd4909d2edcc35c170f67ead9d68d
Mahsa <haghnevismahsa@gmail.com>
2024-07-30 10:30:22 +0330

    feat: add priemter function
    this function calculate the priemter of the shape

--- Shapes/circle/circleModel.swift
+++ Shapes/circle/circleModel.swift
@@ -12,4 +12,13 @@ class CircleModel{
     private (set) var radius : Double
     private let pi = (Double( Double.pi * 1000 )/1000)
     
+    init ( radius : Double ){
+            self.radius = radius
+        }
+    
+    func perimeter () -> Double {
+            return self.radius * self.pi * 2
+        }
+    
+        
 }
