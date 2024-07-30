07c61370cda1be6b8abb3235ffc3ef8b8cabc43f
Aref <arefshal@yahoo.com>
2024-07-30 14:58:03 +0330

    Rename Shape protocol to Shapeable for better naming convention

--- Shapes/Rectangle.swift
+++ Shapes/Rectangle.swift
@@ -7,20 +7,20 @@
 
 import Foundation
 // Define the Shape protocol with area and perimeter methods
-protocol Shape {
+protocol Shapeable {
     func area() -> Double
     func perimeter() -> Double
 }
 
 // Extend the Shape protocol to provide default implementation
-extension Shape {
+extension Shapeable {
     func description() -> String {
         return "Area: \(area()), Perimeter: \(perimeter())"
     }
 }
 
 // Rectangle class conforming to the Shape protocol
-class Rectangle: Shape {
+class Rectangle: Shapeable {
     var width: Double
     var height: Double
     
