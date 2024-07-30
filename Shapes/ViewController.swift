415f1a28c4dd44c3d229ea13e48b12525d77fa4e
Mahdi <125703793+mahdi-mahmoudkhani@users.noreply.github.com>
2024-07-30 09:38:20 +0330

    Initial commit

---
+++ Shapes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Shapes
+//
+//  Created by Mahdi on 5/9/1403 AP.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
