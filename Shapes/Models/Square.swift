0481ef2f231444c9df4a39c32ae8eb02097d4e93
Mahdi <125703793+mahdi-mahmoudkhani@users.noreply.github.com>
2024-07-30 10:06:22 +0330

    feat: Add SquareModel class with side length property
    - Implement SquareModel class with a private (set) side property.
    - Provide an initializer with a default side length of 0.

---
+++ Shapes/Models/Square.swift
@@ -0,0 +1,19 @@
+//
+//  Square.swift
+//  Shapes
+//
+//  Created by Mahdi on 5/9/1403 AP.
+//
+
+import Foundation
+
+class SquareModel {
+    
+    private (set) var side: Double
+    
+    init(withSide side: Double = 0) {
+        self.side = side
+    }
+
+}
+
