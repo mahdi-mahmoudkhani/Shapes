d45ca2539e48e59615cfe1a2342c37517f6fa9c8
Aref <arefshal@yahoo.com>
2024-08-02 20:58:14 +0330

    feat(RectangleViewController): setup buttons

--- Shapes/RectangleViewController.swift
+++ Shapes/RectangleViewController.swift
@@ -116,19 +116,28 @@ class RectangleViewController: UIViewController ,UITextFieldDelegate {
        
     }
     /// Setup the buttons
-        private func setupButtons() {
-            configureButton(addButton, title: "Add", action: #selector(addShape))
-            configureButton(calculateButton, title: "Calculate", action: #selector(calculateResults))
-        }
-        
-        /// Configure a button with title and action
-        private func configureButton(_ button: UIButton, title: String, action: Selector) {
-            button.configuration = createButtonConfiguration(title: title)
-            button.addTarget(self, action: action, for: .touchUpInside)
-            stackView.addArrangedSubview(button)
-        }
+    private func setupButtons() {
+        configureButton(addButton, title: "Add", action: #selector(addShape))
+        configureButton(calculateButton, title: "Calculate", action: #selector(calculateResults))
+    }
 
-    
-    
+    /// Configure a button with title and action
+    private func configureButton(_ button: UIButton, title: String, action: Selector) {
+        button.configuration = createButtonConfiguration(title: title)
+        button.addTarget(self, action: action, for: .touchUpInside)
+        stackView.addArrangedSubview(button)
+    }
+
+    /// Create a configuration for a button
+    private func createButtonConfiguration(title: String) -> UIButton.Configuration {
+        var config = UIButton.Configuration.filled()
+        config.title = title
+        config.baseBackgroundColor = .white
+        config.baseForegroundColor = .black
+        config.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 20, bottom: 10, trailing: 20)
+        config.cornerStyle = .medium
+        return config
+    }
+
     
 }
