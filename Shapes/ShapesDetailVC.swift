a09ace79159def106945cb13263b283097dd456c
Mahdi <125703793+mahdi-mahmoudkhani@users.noreply.github.com>
2024-08-03 10:32:00 +0330

    feat(storyboard): Improve navigation and add detail view
    - Embedded a navigation controller with large titles
    - Set the title of the main view controller to Shapes
    - Renamed the  file and class to
    - Renamed the Shape scene class to
    - Created a new view controller, Shapes DetailVC, and linked it to a corresponding Swift file

---
+++ Shapes/ShapesDetailVC.swift
@@ -0,0 +1,29 @@
+//
+//  ShapesDetailVC.swift
+//  Shapes
+//
+//  Created by Mahdi on 5/13/1403 AP.
+//
+
+import UIKit
+
+class ShapesDetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Shapes/ShapesTableVC.swift
+++ Shapes/ShapesTableVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ShapesTableVC.swift
 //  Shapes
 //
 //  Created by Mahdi on 5/9/1403 AP.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ShapesTableVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
