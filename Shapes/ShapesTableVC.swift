2ece4d152402474891ca524ae6d4b5044eda9d4a
Mahdi <125703793+mahdi-mahmoudkhani@users.noreply.github.com>
2024-08-03 11:21:56 +0330

    feat(tableview): Configure Shapes scene with table view and implement data source
    - Main Storyboard:
      - Added a table view to the Shapes scene
      - Set cell identifier to
      - Configured table view's delegate and data source outlets to
    
    - ShapesTableVC file:
      - Added array to store shape names
      - Implemented and extension
        - to return the count of
        - to configure cell text with data from
        - to perform segue with identifier
      - Configured table view to use identifier for cells

--- Shapes/ShapesTableVC.swift
+++ Shapes/ShapesTableVC.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ShapesTableVC: UIViewController {
 
+    var shapeNames: [String] = ["Square"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -17,3 +19,21 @@ class ShapesTableVC: UIViewController {
 
 }
 
+extension ShapesTableVC: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return shapeNames.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ShapesCell") else {
+            return UITableViewCell()
+        }
+        cell.textLabel?.text = shapeNames[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "ToShapesDetailVC", sender: nil)
+    }
+}
